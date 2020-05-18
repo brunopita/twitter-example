e93e2e794d15d2ae1822facc249ebf01265e418b
Bruno Ramalho <bruno.ramalho@valemobi.com.br>
2020-05-17 21:11:30 -0300

    Adicionando APM para chamada no banco

--- twitter-pg/go.mod
+++ twitter-pg/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
 	github.com/lib/pq v1.5.2
+	go.elastic.co/apm/module/apmsql v1.8.0
 )
--- twitter-pg/spg/spg.go
+++ twitter-pg/spg/spg.go
@@ -4,13 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"go.elastic.co/apm/module/apmsql"
+	_ "go.elastic.co/apm/module/apmsql/pq"
 )
 
 func GetConnection(host, port, user, password, dbname string) (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := apmsql.Open("postgres", psqlInfo)
 	if err != nil {
 		return nil, err
 	}
